fc2545e836e720b8c35272de5043c0d1889db550
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-15 09:20:13 -0300

    refactor: refatorando  model e migrations das categorias, para ficar no padrão sequelize

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
+  class Category extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  BlogPosts.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    userId: DataTypes.INTEGER
+  Category.init({
+    name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'BlogPosts',
+    modelName: 'Category',
   });
-  return BlogPosts;
+  return Category;
 };
