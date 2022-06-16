3e26dc63a64acb4915cdcb8270e03751ab517f18
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-16 09:57:37 -0300

    refactor: refatorando model User para usar função

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,26 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define("User", {
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+  }, { timestamps: false });
+
+  return UserTable;
 };
+
+module.exports = UserSchema;
