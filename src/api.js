b7db88579790287b9d8a442cc60898372e62d338
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-17 16:16:58 -0300

    feat: conectando a rota login com o arquivo

--- src/api.js
+++ src/api.js
@@ -1,17 +1,15 @@
 const express = require('express');
 
-// ...
-
 const app = express();
 
 app.use(express.json());
+
+const loginRouter = require('./routers/loginRouter');
+
+app.use('/login', loginRouter);
 
 app.use((err, _req, res, _next) => {
   res.status(err.statusCode || 500).json({ message: err.message });
 });
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
