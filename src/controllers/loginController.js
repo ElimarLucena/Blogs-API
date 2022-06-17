e5ba18a61325d414182170d444236a55f26fa90a
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-17 15:41:04 -0300

    feat: retornando token ou messagem de erro no loginController

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,13 @@
-// const services = require('../services/loginService');
+const services = require('../services/loginService');
 
-const authenticate = (_req, _res) => {
+const authenticate = async (req, res) => {
+  const token = await services.authenticate(req.body);
 
+  if (token) {
+    return res.status(400).json({ message: 'Invalid fields' });
+  }
+
+  return res.status(200).json(token);
 };
 
 module.exports = {
