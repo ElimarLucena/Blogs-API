b8f12ff47424e64f056941171c59f352d9acbadf
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-17 16:04:39 -0300

    feat: criando middleware de erro no arquivo api

--- src/api.js
+++ src/api.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use((err, _req, res, _next) => {
+  res.status(err.statusCode || 500).json({ message: err.message });
+});
+
 // ...
 
 // É importante exportar a constante `app`,
