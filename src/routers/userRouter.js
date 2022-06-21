bd59726869d500ba6133967aa27578a2dd832bee
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-21 07:55:40 -0300

    feat: criando rota do tipo get com essa rota será possível buscar por todos os usuários

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,10 +4,14 @@ const controllers = require('../controllers/userController');
 
 const { userCreationValidation } = require('../middleware/userMiddleware');
 
+const { authenticateToken } = require('../middleware/tokenMiddleware');
+
 const router = express.Router();
 
 router.use(express.json());
 
 router.post('/', userCreationValidation, controllers.createUser);
+
+router.get('/', authenticateToken, controllers.getAllUsers);
 
 module.exports = router;
