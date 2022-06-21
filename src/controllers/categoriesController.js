596f152a4ca8568e2c58d7776d113468f6527264
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-21 10:47:01 -0300

    refactor: adicionando o try/catch a função createcategory adicionando tratamento de erro e adequando retorno da função

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,16 +2,20 @@ const services = require('../services/categoriesService');
 
 const createCategory = async (req, res) => {
   if (!req.body.name) {
-    return res.status(400).json({ message: 'name is required' });
+    return res.status(400).json({ message: '"name" is required' });
   }
 
-  const category = await services.createCategory(req.body);
-
-  if (!category) {
-    return res.status(409).json({ message: 'category already registered' });
+  try {
+    const category = await services.createCategory(req.body);
+  
+    if (!category) {
+      return res.status(409).json({ message: 'category already registered' });
+    }
+  
+    return res.status(201).json(category);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  return res.status(201).json(category);
 };
 
 module.exports = {
