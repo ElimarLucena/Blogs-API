770baa6fcc3fbf65969899975113bf4f798964f1
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-21 14:32:03 -0300

    feat: retornando todas as categorias a função getAllCategories da camada controller, retornas todas as categorias cadastradas no banco

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -18,6 +18,17 @@ const createCategory = async (req, res) => {
   }
 };
 
+const getAllCategories = async (_req, res) => {
+  try {
+    const users = await services.getAllCategories();
+
+    return res.status(200).json(users);
+  } catch (e) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   createCategory,
+  getAllCategories,
 };
