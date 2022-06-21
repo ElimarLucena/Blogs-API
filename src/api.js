6fc8da007bc159d44a9117e3ece33c882de0c7c4
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-21 09:35:53 -0300

    feat: rota categories importando arquivo , onde ficará as rotas de categories

--- src/api.js
+++ src/api.js
@@ -8,9 +8,13 @@ const loginRouter = require('./routers/loginRouter');
 
 const userRouter = require('./routers/userRouter');
 
+const categoriesRouter = require('./routers/categoriesRouter');
+
 app.use('/login', loginRouter);
 
 app.use('/user', userRouter);
+
+app.use('/categories', categoriesRouter);
 
 app.use((err, _req, res, _next) => {
   res.status(err.statusCode || 500).json({ message: err.message });
