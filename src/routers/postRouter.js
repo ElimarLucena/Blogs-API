92d6eab86e16d435b693620de2ac84d81835f932
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-22 07:58:40 -0300

    feat: aplicando  middleware de validação na rota post

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -4,10 +4,15 @@ const controller = require('../controllers/postController');
 
 const { authenticateToken } = require('../middleware/tokenMiddleware');
 
+const { postCreationValidation } = require('../middleware/postMiddleware');
+
 const router = express.Router();
 
 router.use(express.json());
 
-router.post('/', authenticateToken, controller.createBlogPost);
+router.post('/', 
+  authenticateToken, 
+  postCreationValidation, 
+  controller.createBlogPost);
 
 module.exports = router;
