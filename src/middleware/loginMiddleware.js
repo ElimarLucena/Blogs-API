90af4c493757277ee28fd9bd2cf3eed54472a892
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-27 09:01:55 -0300

    refactor: utilizando a lib  no middleware de

--- src/middleware/loginMiddleware.js
+++ src/middleware/loginMiddleware.js
@@ -1,10 +1,25 @@
-const loginValidation = (req, _res, next) => {
-  const { email, password } = req.body;
+const joi = require('joi');
 
-  if (!email || !password) {
+const schemas = () => {
+  const schema = joi.object().keys({
+    email: joi.string().empty('').required(),
+    password: joi.string().empty('').required(),
+  }).messages({ 
+    'any.required': 'Some required fields are missing',
+  });
+
+  return schema;
+};
+
+const loginValidation = (req, _res, next) => {
+  const schema = schemas();
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
     next({
       statusCode: 400,
-      message: 'Some required fields are missing',
+      message: error.details[0].message,
     });
   }
 
